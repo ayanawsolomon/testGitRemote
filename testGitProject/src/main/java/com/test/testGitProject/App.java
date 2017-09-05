cf1c4afb9e1bc934838c5ba8b6e59169e5fd1304
solomon <ayanawsolomon1@gmail.com>
2017-09-04 20:12:08 -0400

    I have deleted hello world print

--- testGitProject/src/main/java/com/test/testGitProject/App.java
+++ testGitProject/src/main/java/com/test/testGitProject/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         System.out.println("hello first commit");
         for(int i=0; i<=10; i++){
         	System.out.println(i);
